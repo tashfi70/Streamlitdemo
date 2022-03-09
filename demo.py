078c61350a2460be6aab876d1488b36e00a1227a
Tasfia Tasnim <54186534+tashfi70@users.noreply.github.com>
2022-03-09 15:00:45 +0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,7 @@
+import streamlit as st
+st.set_page_config(page_title="Cyberbully Detection Demo Website", page_icon=":tada:", layout="wide")
 
+#HEADER
+st.subheader("Trial for making website")
+st.title("title trial")
+st.write("writing trial")
