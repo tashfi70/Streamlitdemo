bb206811c0ae17ff86786a0299967be8b31a8858
Tasfia Tasnim <54186534+tashfi70@users.noreply.github.com>
2022-03-11 21:34:01 +0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,43 @@
+#import streamlit as st
+#st.set_page_config(page_title="Cyberbully Detection Demo Website", page_icon=":tada:", layout="wide")
+
+#HEADER
+#st.subheader("Trial for making website")
+#st.title("title trial")
+#st.write("writing trial")
+
+
 import streamlit as st
+#import pip
+#import selenium
+#from tensorflow import keras
+
+
 st.set_page_config(page_title="Cyberbully Detection Demo Website", page_icon=":tada:", layout="wide")
 
 #HEADER
 st.subheader("Trial for making website")
 st.title("title trial")
 st.write("writing trial")
+
+header = st.container()
+dataset = st.container()
+features = st.container()
+modelTraining = st.container()
+
+with header:
+	st.title("Trial for making website")
+
+title = st.text_input('Reply to', 'your txt')
+st.write('You replied', title)
+
+
+
+#!pip install tweet-preprocessor
+#!pip install contractions
+
+#!pip install gensim
+
+from tensorflow import keras
+model = keras.models.load_model('model.h5')
+
